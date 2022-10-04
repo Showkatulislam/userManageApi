4175cd85f4f4279f860496b71d634243d39f9066
Showkatul islam <showkatul2@gmail.com>
2022-10-04 22:35:26 +0600

    env

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const UserRoutes =require('./routes/users.routes')
-
+require('dotenv').config()
 const app=express()
-
+console.log(process.env)
 //middleware 
 app.use(express.json())
 app.use(cors())
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-
-app.listen(8000,()=>{
+const port=process.env.PORT ||8000
+app.listen(port,()=>{
     console.log("Server is running from port https://localhost:8000")
 })
